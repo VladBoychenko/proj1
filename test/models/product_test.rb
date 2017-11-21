4c4ad541b3a8d2a1df5ccdfcb9bd5d6acc94032b
VladBoychenko <cmpun14@gmail.com>
2017-11-22 01:08:49 +0200

    Continum

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -21,15 +21,6 @@ assert product.invalid?
 assert_equal ["has already been taken"], product.errors[:title]
 # уже было использовано
 end
-test " product is not valid without a unique title - i18n" do
-product = Product.new(title: products(:ruby).title,
-description: "yyy",
-price: 1,
-image_url: "fred.gif")
-assert product.invalid?
-assert_equal [I18n.translate('activerecord.errors.messages.taken')],
-product.errors[:title]
-end
 # assert true
 # end
 end
