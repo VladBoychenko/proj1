dac261b572053abe71c31c6a328d7d0626f9875f
VladBoychenko <cmpun14@gmail.com>
2017-11-27 19:36:56 +0200

    AJAX

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -30,7 +30,8 @@ before_action :set_line_item, only: [:show, :edit, :update, :destroy]
       @line_item = @cart.add_product(product.id)
       respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart }
+        format.html { redirect_to store_url }
+        format.js { @current_item = @line_item }
         format.json { render action: 'show', 
             status: :created, location: @line_item }
       else
