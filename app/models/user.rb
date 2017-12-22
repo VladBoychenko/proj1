9d43e9fb8a625d3b658e4fc9e8bc529ae94510bb
VladBoychenko <cmpun14@gmail.com>
2017-12-22 17:13:15 +0200

    Admin2

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ after_destroy :ensure_an_admin_remains
 
 private
     def ensure_an_admin_remains
-        if User.count.zero?
-            raise "Последний пользователь не может быть удален"
-        end
+    if User.count.zero?
+        raise "Последний пользователь не может быть удален"
     end
+  end
 end
